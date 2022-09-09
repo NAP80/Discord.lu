1c01dcd5aa8d41876c173edb04c72f3f0a8bcd21
Pie Bleu <napiebleu@gmail.com>
2022-09-10 00:01:46 +0200

    Correction Variable.

--- ihm/map/affichageAutrePersos.php
+++ ihm/map/affichageAutrePersos.php
@@ -4,13 +4,13 @@
         if(count($listPersos) > 1){
             ?>
                 <div class='divInfoPlayers'>
-                    <p class='pInfoPlayers'>Visiblement tu n'es pas seul ici il y a aussi :</p>
+                    <p class='pInfoPlayers'>Visiblement tu n'es pas seul ici, il y a aussi :</p>
                 </div>
                 <ul id="ulPersos" class="ulPersonnages">
                     <?php
-                        $PersoJoeuur = $Joueur1->getPersonnage();
-                        foreach($listPersos as  $Perso){
-                            if($Perso->getId()!=$PersoJoeuur->getId()){
+                        $PersoJoueur = $Joueur1->getPersonnage();
+                        foreach($listPersos as $Perso){
+                            if($Perso->getId()!=$PersoJoueur->getId()){
                                 ?>
                                     <li class="liAdverse" onmouseover="afficheDivPerso(event)" onmouseout="cacheDivPerso(event)">
                                         <a id="aPerso<?= $Perso->getId() ?>" onclick="AttaquerPerso(<?= $Perso->getId() ?>,0, event)">
