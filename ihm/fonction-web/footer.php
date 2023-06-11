15b07fedf5fa5378c097247a86865274448f9724
Pie Bleu <napiebleu@gmail.com>
2023-06-11 11:31:09 +0200

    Ortho

--- ihm/fonction-web/footer.php
+++ ihm/fonction-web/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="divFooter">
-        <p>Site actuellement en dévelopement. En cas de problème, bug, ou question, contactez l'équipe sur le <a href="https://github.com/NAP80/Discord.lu">Github</a>.</p>
+        <p>Site actuellement en développement. En cas de problème, bug, ou question, contactez l'équipe sur le <a href="https://github.com/NAP80/Discord.lu">Github</a>.</p>
         <p class="Copyright1">Copyright : 2022 - <?= date("Y") ?> - Tous droits réservés.</p>
         <!--<p class="Copyright2">Réalisé par <a href="https://github.com/NAP80/">NAP80</a> : <a href="https://github.com/NAP80/Discord.lu">github.com/NAP80/Discord.lu</a>.</p>-->
     </div>
